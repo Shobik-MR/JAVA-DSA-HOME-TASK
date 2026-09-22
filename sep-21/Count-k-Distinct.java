636b98c8df01d99d141475b3fa68843d4ab91c22
Shobik-don <shobikshobik504@gmail.com>
2026-09-22 19:55:42 +0530

    sep-21

---
+++ sep-21/Count-k-Distinct.java
@@ -0,0 +1,22 @@
+import java.util.*;
+//  class Solution {
+//     ArrayList<Integer> countDistinct(int arr[], int k) {
+//         // code here
+//         int count =0;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         ArrayList<Integer> list1 = new ArrayList<>();
+//         for(int i=0,j=0;i<arr.length;i++){
+           
+//             list.add(arr[i]);
+          
+//             if(list.size()==k){
+                
+//                 set.addAll(list);
+//                 list1.add(set.size());
+//                 list.remove(0);
+//             }
+            
+//         }
+//         return list1;
+//     }
+// }
